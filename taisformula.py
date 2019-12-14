ffd07b0be6b032c7e437d89f6ba5eb7d24301856
velvetarchangel <32275484+velvetarchangel@users.noreply.github.com>
2019-12-14 16:17:20 -0700

    Add files via upload

---
+++ taisformula.py
@@ -0,0 +1,18 @@
+#Tai's formula
+
+N = int(input())
+array = []
+glucose_reading = 0
+for i in range(N):
+    arr = [0]*2
+    t, g = [float(x) for x in input().split()]
+    arr[0] = t
+    arr[1] = g
+    array.append(arr)
+    arr = [0]*2
+
+#print(array)
+for i in range(len(array)-1):
+    glucose_reading += (((array[i][1] + array[i+1][1])/2)*(array[i+1][0]-array[i][0])/1000)
+    #print(glucose_reading)
+print(glucose_reading)
